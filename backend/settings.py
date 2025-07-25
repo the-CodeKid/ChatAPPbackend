fa8f87379fbb2747cf3bf5543e4d61909a2d332e
the-CodeKid <sagarkt98@gmail.com>
2025-07-26 01:38:43 +0530

    Bug fix #1

--- backend/settings.py
+++ backend/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest-framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ]
 }
 
